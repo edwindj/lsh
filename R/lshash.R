751ef7fbb4ed486a56949a82dcf9886e3ae7a2e6
Edwin de Jonge <edwindjonge@gmail.com>
2014-03-26 16:26:24 +0100

    trying

--- R/lshash.R
+++ R/lshash.R
@@ -6,10 +6,18 @@
 #' @param L number of hashtable
 #' @param ... not used
 #' @export
-lshash <- function(data, k=10, L=min(ncol(data), 10),  ...){
+lshash <- function(data, k=32, L=min(ncol(data), 10),  ...){
 }
 
-gen_rnd_projection <- function(dim, k, ...){
-  matrix(runif(dim*k), ncol=k)
+gen_rnd_projection <- function(dim, k=32, ...){
+  m <- matrix(rnorm(dim*k), ncol=dim)
+  m/rowSums(m)
 }
+
+
+d <- as.matrix(iris[1:4])
+a <- tcrossprod(d, gen_rnd_projection(4)) > 0
+h <- apply(a, 1, function(r) {as.bit(r)})
+iris$h <- h
+View(iris)
 
