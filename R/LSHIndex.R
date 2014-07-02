ebe947c32bd802d4b8d0e5d5646eb17dc94dc230
Edwin de Jonge <edwindjonge@gmail.com>
2014-07-03 00:33:04 +0200

    wip

--- R/LSHIndex.R
+++ R/LSHIndex.R
@@ -7,22 +7,28 @@ LSHIndex <- setRefClass("LSHIndex",
                , basis = "array"
                , basis_m = "matrix"
                , dim="integer"
-               , index="list"
+               , index="ANY"
                , L="integer"
                , k="integer"
+               , w="integer"
    ),
    methods=list(
      initialize = function(.data, L=10L, k=min(10L, ncol(.data)), w=32L){
-       data <<- .data
+       data <<- as.matrix(.data)
        dim <<- ncol(data)
        L <<- as.integer(L)
        k <<- as.integer(k)
        basis_m <<- matrix(rnorm(dim*L*k), ncol=k*L, nrow=dim)
        basis <<- basis_m
        dim(basis) <<- c(dim,k,L)
+       w <<- w
+       index <<- reindex()
      },
      reindex = function(){
-       tcrossprod(basis, data)
+       d <- data %*% basis_m
+       l <- apply(d, 2, function(x) unclass(cut(x, w)))
+       dim(l) <- c(nrow(d), k, L)
+       l
      }
    )
 )
