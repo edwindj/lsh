2c597ba41d13f5b6762f06fb7a628b06e4f93106
Edwin de Jonge <edwindjonge@gmail.com>
2014-07-03 00:35:45 +0200

    wip

--- R/knn_lsh.R
+++ R/knn_lsh.R
@@ -2,8 +2,8 @@
 #' 
 #' @export
 knn_lsh <- function(data, L=10, k=10, lsh_index=NULL, distance=NULL){
+  data_index <- LSHIndex(data, L=L, k=k)
   if (is.null(lsh_index)){
-    lsh_index <- LSHIndex(data, L=L, k=k)
+    lsh_index <- data_index
   }
-  
 }
