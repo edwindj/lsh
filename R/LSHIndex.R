aa1e98e41bc7da18b6615caea870513b937e397a
Edwin de Jonge <edwindjonge@gmail.com>
2014-07-02 21:30:42 +0200

    updated class

--- R/LSHIndex.R
+++ R/LSHIndex.R
@@ -1,11 +1,14 @@
 #' LSHIndex
 #'
-#' @export
+#' LSHIndex
+#' @export LSHIndex
 LSHIndex <- setRefClass("LSHIndex",
-   fields=c("data", "index"),
+   fields=list(data="ANY", basis="array", dim="integer", index="list"),
    methods=list(
      initialize = function(.data, L=10, k=10, w=32){
        data <<- .data
+       dim <<- ncol(data)
+       basis <<- array(rnorm(dim*L*k), dim = c(dim, L, k))
      }
    )
 )
--- R/knn_lsh.R
+++ R/knn_lsh.R
@@ -2,5 +2,8 @@
 #' 
 #' @export
 knn_lsh <- function(data, L=10, k=10, lsh_index=NULL, distance=NULL){
+  if (is.null(lsh_index)){
+    lsh_index <- LSHIndex(data, L=L, k=k)
+  }
   
 }
