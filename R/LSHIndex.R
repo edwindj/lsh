36145df1f2913e597afb68c03781bc0d5ec5231d
Edwin de Jonge <edwindjonge@gmail.com>
2014-07-02 22:15:05 +0200

    tweaking

--- R/LSHIndex.R
+++ R/LSHIndex.R
@@ -3,12 +3,25 @@
 #' LSHIndex
 #' @export LSHIndex
 LSHIndex <- setRefClass("LSHIndex",
-   fields=list(data="ANY", basis="array", dim="integer", index="list"),
+   fields=list(  data = "ANY"
+               , basis = "array"
+               , basis_m = "matrix"
+               , dim="integer"
+               , index="list"
+               , L="integer"
+               , k="integer"
+   ),
    methods=list(
      initialize = function(.data, L=10, k=10, w=32){
        data <<- .data
        dim <<- ncol(data)
+       L <<- L
+       k <<- k
        basis <<- array(rnorm(dim*L*k), dim = c(dim, L, k))
+     },
+     index = function(data){
+       tcrossprod
      }
    )
 )
+
